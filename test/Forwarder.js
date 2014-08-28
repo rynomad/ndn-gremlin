e2d327cf697cb5abef92c3f4484c502fb113eec7
Ryan Bennett <nomad.ry@gmail.com>
2014-08-27 20:19:48 -0600

    crossplatform Forwarder tests

--- test/Forwarder.js
+++ test/Forwarder.js
@@ -1,5 +1,6 @@
+var ndn = require("ndn-contrib").ndn
 module.exports = function(Forwarder, assert, runtime){
-  var forwarder;
+  var forwarder, listenerRes = {};
 
   describe("Forwarder", function(){
     forwarder = new Forwarder();
@@ -58,14 +59,12 @@ module.exports = function(Forwarder, assert, runtime){
       })
 
       it("should accept a listener on a prefix of existing", function(){
-        forwarder.addListener({prefix:"crossPlatform", blocking: true}, function(){
-          return "/crossPlatform";
-        })
-        forwarder.addListener("crossPlatform/blocking/test", function(){
-          return "/crossPlatform";
-        })
-        forwarder.addListener({prefix: "crossPlatform/blocking", blocking: true}, function(){
-          return "/crossPlatform";
+        forwarder.addListener({prefix:"crossPlatform", blocking: true}, function(interest, faceID, unblock){
+          listenerRes.hasReachedEnd = true;
+          return {
+            blocking: true,
+            unblock: unblock
+          };
         })
         assert(forwarder.listeners.nameTree["/crossPlatform"].fibEntry.nextHops.length === 1)
 
@@ -83,18 +82,219 @@ module.exports = function(Forwarder, assert, runtime){
         assert(forwarder.listeners.nameTree["/crossPlatform"].fibEntry.nextHops.length === 1)
       })
       it("should remove item from callback array", function(){
-        assert(forwarder.listenerCallbacks[0] === null)
+        assert(forwarder.listenerCallbacks[0].callback === null)
       })
       it("should not disturb listener callback order", function(){
         assert(forwarder.listenerCallbacks[1].callback() === "crossPlatform/test/object/blocking" )
-        assert(forwarder.listenerCallbacks[3].callback() === "/crossPlatform")
+        assert(forwarder.listenerCallbacks[3].callback().blocking === true)
       })
     })
 
+    describe("Forwarder.handleInterest", function(){
+      describe("triggers listeners correctly", function(){
+
+        before(function(){
+          forwarder.addListener({
+            blocking: true
+            , prefix: "crossPlatform/blocking/test/ordering/top"
+          }, function(interest, faceID, unblock){
+            listenerRes.firstBlockingTriggered = true ; //this should be superceded by next blocking listener, should not be true
+            unblock();
+          })
+          .addListener(
+            "crossPlatform/blocking/test/ordering/top"
+            , function(interest, faceID){
+              listenerRes.firstNonBlockingTriggered = true; //should be triggered
+            }
+          )
+          .addListener(
+            {
+              prefix : "crossPlatform/blocking/test/ordering/top"
+              , blocking: true
+            }
+            , function(interest, faceID, unblock){
+              listenerRes.secondBlockingTriggered = true // THIS should be triggered
+              listenerRes.unblockFirst = unblock;
+            }
+          )
+          .addListener(
+            "crossPlatform/blocking/test/ordering/top"
+            , function(interest, faceID){
+              listenerRes.secondNonBlockingTriggered = true; // should be triggered regardless of add order
+            }
+          )
+          .addListener(
+            {
+              prefix: "crossPlatform/blocking/test/ordering"
+              , blocking: true
+            }
+            , function(interest, faceID, unblock){
+              listenerRes.unblockSecond = unblock
+              listenerRes.firstPrefixTriggered = true;
+            }
+          ).addListener(
+            "crossPlatform/blocking/test"
+            , function(interest, faceID){
+              console.log("WHERE???????????????????????????????????????????????????????????????????????????")
+              listenerRes.SecondPrefixTriggered = true;
+            }
+          )
+
+        })
+
+
+        it("should trigger all nonBlocking Listeners on longest prefix", function(){
+          var inst = new ndn.Interest(new ndn.Name("crossPlatform/blocking/test/ordering/top"))
+          var element = inst.wireEncode();
+          forwarder.handleInterest(element, 0);
+          console.log("got past handleInterest")
+          assert(listenerRes.firstNonBlockingTriggered);
+          assert(listenerRes.secondNonBlockingTriggered);
+        })
+        it("should block with most recently added blocking listener on listener entry", function(){
+
+          assert(!listenerRes.firstBlockingTriggered);
+          assert(listenerRes.secondBlockingTriggered);
+          assert(!listenerRes.firstPrefixTriggered)
+        })
+        it("should unblock", function(){
+          console.log(listenerRes.unblockFirst.toString())
+          listenerRes.unblockFirst();
+
+          assert(listenerRes.firstPrefixTriggered);
+
+
+        })
+        it("should reblock", function(){
+          assert(!listenerRes.SecondPrefixTriggered, "second prefix should be blocked");
+        })
+        it("should unblock with skipListen param", function(){
+          listenerRes.unblockSecond(true);
+          assert(!listenerRes.SecondPrefixTriggered, "second prefix should be skipped")
+        })
+      })
+      /*
+
+      */
+      describe("forward interests correctly", function(){
+        var saveDispatch = forwarder.interfaces.dispatch
+
+        before(function(){
+          forwarder.fib
+          .addEntry("test/forwarding/long", 5)
+          .addEntry("test/forwarding/long", 4)
+          .addEntry("test/forwarding", 3)
+          .addEntry("test/forwarding/long/er", 2);
+        })
+
+        it("should dispatch with expected faceFlag", function(){
+          var inst = new ndn.Interest(new ndn.Name("test/forwarding/long"))
+          var element = inst.wireEncode();
+          console.log(element)
+          forwarder.interfaces.dispatch = function(element, faceFlag){
+            console.log("DISPATCH: " + faceFlag)
+            assert((faceFlag & (1<<5)), "5th bit should be set")
+            assert((faceFlag & (1<<4)), "4th bit should be set")
+            assert((faceFlag & (1<<3)), "3rd bit should be set")
+            assert(!(faceFlag & (1<<2)), "2nd bit should NOT be set")
+          }
+          forwarder.handleInterest(element, 0)
+
+        })
+
+        it("should not dispatch to face that data was received on", function(){
+          var inst = new ndn.Interest(new ndn.Name("test/forwarding/long"))
+          var element = inst.wireEncode();
+          forwarder.interfaces.dispatch = function(element, faceFlag){
+            assert((faceFlag & (1<<5)), "5th bit should be set")
+            assert(!(faceFlag & (1<<4)))
+            assert((faceFlag & (1<<3)))
+            assert(!(faceFlag & (1<<2)))
+          }
+          forwarder.handleInterest(element, 4);
+        })
+
+        after(function(){
+          forwarder.interfaces.dispatch = saveDispatch;
+        })
+      })
+
+      describe("return cacheHits correctly", function(){
+        var saveDispatch = forwarder.interfaces.dispatch
+
+        before(function(){
+          var data = new ndn.Data(new ndn.Name("/cache/hit/test"), new ndn.SignedInfo(), "correct")
+          data.signedInfo.setFreshnessPeriod(1000)
+          data.signedInfo.setFields();
+          data.sign()
+          var element = data.wireEncode();
+          forwarder.cache.insert(element, data)
+
+        })
+        it("should trigger dispatch with same faceID, and a raw data packet", function(){
+          forwarder.interfaces.dispatch = function(element, faceFlag){
+            assert((faceFlag & (1<<2)))
+            var data = new ndn.Data()
+            data.wireDecode(element)
+
+            assert(data.name.toUri() === "/cache/hit/test");
+          }
+          var inst = new ndn.Interest(new ndn.Name("cache/hit/test"))
+          var element = inst.wireEncode();
+          forwarder.handleInterest(element, 2)
+        })
+        after(function(){
+          forwarder.interfaces.dispatch = saveDispatch;
+        })
+      })
+    })
+
+
     describe("Forwarder.handleData", function(){
+      var saveDispatch = forwarder.interfaces.dispatch;
 
-      it()
+      var inst = new ndn.Interest(new ndn.Name("forward/test"))
 
+      inst.setInterestLifetimeMilliseconds(1000);
+      var element = inst.wireEncode()
+      var data = new ndn.Data(new ndn.Name("forward/test/matching"), new ndn.SignedInfo(), "success")
+      data.signedInfo.setFields()
+      data.sign()
+      var dataElement = data.wireEncode();
+
+      before(function(){
+        forwarder.pit.insertPitEntry(inst.wireEncode(), inst, 9)
+      })
+
+      it("should dispatch to matching pit entries", function(){
+        forwarder.interfaces.dispatch = function(element, faceFlag){
+          assert((faceFlag & (1<<9)))
+        }
+        forwarder.handleData(dataElement, 8);
+      })
+
+      it("should not dispatch if no pit entries (or duplicate)", function(){
+        forwarder.interfaces.dispatch = function(element, faceFlag){
+          console.log(faceFlag)
+          assert(false)
+        }
+        forwarder.handleData(dataElement, 8)
+      })
+
+      it("should cache", function(){
+        forwarder.interfaces.dispatch = function(eelement, faceFlag){
+          assert((faceFlag & (1<<10)))
+          var d = new ndn.Data()
+          d.wireDecode(eelement);
+          assert(d.name.toUri() === "/forward/test/matching")
+        }
+        forwarder.handleInterest(element, 10)
+
+      })
+      after(function(){
+        forwarder.interfaces.dispatch = saveDispatch;
+
+      })
     })
     runtime(forwarder);
 
--- test/node/suite.js
+++ test/node/suite.js
@@ -1,11 +1,2 @@
-var Forwarder = require("../../src/Forwarder.js")
-  , assert = require('assert')
-  , nodeTests = function(forwarder){
-    it("should execute within Forwarder section", function(){
-      assert(true);
-      assert(forwarder.nameTree);
-    })
 
-
-  };
-require("../Forwarder.js")(Forwarder, assert, nodeTests)
+require("./forwarder.js")()
